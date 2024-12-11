7cea1e290829af6561bcabc69bc1f4cd1e96a216
Megha Balaraju <42817282+meghabalaraju@users.noreply.github.com>
2024-12-11 17:42:33 +0100

    PR One (#1)
    * change text on the main page
    
    * separate the workflows
    
    * combine dockerizing and running tests steps
    
    * fix branches pattern
    
    * test
    
    * use docker tocken for interactive login
    
    * fix typo
    
    * fiy typo
    
    * tests 1
    
    * push image to hub
    
    * move params to env variables
    
    * check secrets retrieval
    
    * test
    
    * fix passing secrets issues

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn react
+          It is changes on the feature branch
         </a>
       </header>
     </div>
